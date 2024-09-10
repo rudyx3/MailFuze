84b7b8adb239443b9f28bf5a070b940d2ff8bc4c
Rudraa <rudybbx3@gmail.com>
2024-09-10 13:05:56 +0400

    Designs for List

--- backend/models/userLists.js
+++ backend/models/userLists.js
@@ -1,2 +1,40 @@
-//rudraa 
+//rudraa
 //create adding table schema to the db
+
+//rudraa
+//create adding table schema to the db
+
+import pool from "../db/config_db.js";
+
+const createUserLists = async () => {
+  try {
+    await pool.connect();
+    await pool.query(`
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    `);
+    await pool.query(`
+        CREATE TABLE userlists (
+            listId UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+            userId VARCHAR(255) NOT NULL,
+            listName VARCHAR(100) NOT NULL,
+            senderEmail VARCHAR(100) DEFAULT NULL, 
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    
+    `);
+
+    console.log("Table Created");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addAuserLIst = async () => {
+  await pool.connect();
+  await pool.query(`
+    INSERT INTO userlists(userId, listName, senderEmail) VALUES('123912','test list', 'rudraapatel@gmail.com');
+`);
+  console.log("addded a record");
+};
+
+addAuserLIst();
